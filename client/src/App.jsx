e506e27582342cc9a25b80638b4d9b308f2a3c30
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-05 13:48:31 +0000

    short

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   ) 
 }
