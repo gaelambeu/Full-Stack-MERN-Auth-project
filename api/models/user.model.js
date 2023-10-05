d454ed81426e838cb05e70d4629a8dcb0c1b5f7e
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-05 22:05:46 +0000

    short

---
+++ api/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+})
