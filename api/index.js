aa018c300f5880e104d958d4c21170930f49456e
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-05 22:16:20 +0000

    short

--- api/index.js
+++ api/index.js
@@ -15,3 +15,10 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server listening on port 3000!')
 })
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'API is workking',
+    })
+})
