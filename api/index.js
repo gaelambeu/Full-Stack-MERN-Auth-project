f6b5161a9050888ffe87b8477c14d32676da1d2d
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-05 22:54:35 +0000

    api sign up is correct

--- api/index.js
+++ api/index.js
@@ -15,6 +15,8 @@ mongoose.connect(process.env.MONGO).then(() =>{
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server listening on port 3000!')
 })
