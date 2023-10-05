8201558982fe694872198f0377e56198fe294557
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-05 14:05:20 +0000

    update

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='bg-slate-200'>
+        <div className="font-bold">
+            <h1>Auth App</h1>
+        </div>
+    </div>
+  )
+}
