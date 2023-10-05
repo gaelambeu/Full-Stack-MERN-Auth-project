6025f27e698b88b7523268a4da9a95667ac77f47
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-05 22:00:00 +0000

    mongo db connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-mongoose.connect(process.env.MONGO)
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log('connected to MongoDB');
+}).catch((err) => {
+    console.log(err)
+});
 
 const app = express();
 
