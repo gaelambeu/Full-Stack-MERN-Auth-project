7fc5dfe2eca582eaf7951146fedbcca0afb14741
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-05 14:27:30 +0000

    short

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,11 +5,20 @@ export default function Header() {
   return (
     <div className='bg-slate-200'>
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
-            <h1 className="font-bold">Auth App</h1>
+            <Link to='/'>
+                <h1 className="font-bold">Auth App</h1>
+            </Link>
+            
             <ul className='flex gap-4'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Sign In</li>
+                <Link to='/'>
+                    <li>Home</li>
+                </Link>
+                <Link to='/about'>
+                    <li>About</li>
+                </Link>
+                <Link to='/sign-in'>
+                    <li>Sign In</li>
+                </Link>
             </ul>
         </div>
     </div>
