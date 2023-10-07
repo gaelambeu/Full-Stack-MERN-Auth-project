3804ff80f6be6c6575973c66daf7a4bc0e5e5cbd
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-07 19:45:18 +0000

    short

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function SignUp() {
   return (
@@ -8,8 +8,14 @@ export default function SignUp() {
         <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg' />
         <input type="email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' />
         <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase'>Sign Up</button>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
       </form>
+      <div className="">
+        <p>Have an account</p>
+        <Link to='/sign-in'>
+          <span className='text-blue-500'>Sign in</span>
+        </Link>
+      </div>
     </div>
   )
 }
