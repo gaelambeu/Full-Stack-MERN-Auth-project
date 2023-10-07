e25bc5474b2485d53cdf99da5ee585b727029011
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-07 19:22:17 +0000

    short

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -10,6 +10,6 @@ export const signup = async (req, res, next) => {
     await newUser.save()
     res.status(201).json({message: "User created successfully"})
    } catch (error) {
-    next(errorHandler(300, "something went wrong"));
+    next(error);
    }
 }
