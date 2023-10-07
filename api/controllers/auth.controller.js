7e05714ca6fc6592777409b5ff96ee73d424ece7
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-07 19:18:41 +0000

    short

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
    const { username, email, password } = req.body;
@@ -9,7 +10,6 @@ export const signup = async (req, res, next) => {
     await newUser.save()
     res.status(201).json({message: "User created successfully"})
    } catch (error) {
-    next(error);
+    next(errorHandler(300, "something went wrong"));
    }
-    
 }
