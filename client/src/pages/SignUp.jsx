7409b84a3b8218fb2b997a09b7987f80232effb0
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-07 21:08:26 +0000

    short

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,21 +4,29 @@ import { Link } from 'react-router-dom'
 export default function SignUp() {
 
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const handleChange = (e) => {
     setFormData ({ ...formData, [e.target.id]: e.target.value});
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch ('/api/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    console.log(data); 
+    try {
+      setLoading(true);
+      const res = await fetch ('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      setError(false);
+    } catch (error) {
+      
+    }
   };
    
   return (
