3f517ab03e3a35722195c975d3657a8ca0e4f225
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-07 20:27:27 +0000

    short

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,24 @@
-import {Link} from 'react-router-dom'
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
+
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData ({ ...formData, [e.target.id]: e.target.value});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
+   
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-bold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg' />
-        <input type="email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' />
-        <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' />
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
+        <input type="email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
+        <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
       </form>
       <div className='flex gap-2 mt-5'>
