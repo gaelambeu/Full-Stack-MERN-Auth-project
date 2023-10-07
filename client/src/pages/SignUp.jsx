ea8efae2c4db5a79a49b56c8281c8e00904199a1
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-07 19:47:33 +0000

    end front end

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -10,8 +10,8 @@ export default function SignUp() {
         <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' />
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
       </form>
-      <div className="">
-        <p>Have an account</p>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
         <Link to='/sign-in'>
           <span className='text-blue-500'>Sign in</span>
         </Link>
