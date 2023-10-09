fe5337bf279f6a7870130c969909ba836be3fad4
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-09 23:15:21 +0000

    building

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -23,8 +23,15 @@ export const signin = async (req, res, next) => {
       const validUser = await User.findOne({ email });
       if (!validUser) return next(errorHandler(404, 'User not found'));
       const validPassword = bcryptjs.compareSync(password, validUser.password);
+
       if (!validPassword) return next(errorHandler(401, 'wrong credentials'));
-      const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET)
+      const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
+
+      const { password: hashedPassword, ...rest } = validUser._doc;
+      res
+         .cookie('access_token', token, {httpOnly: true})
+         .status(200)
+         .json(rest)
    } catch (error) {
       next(error)
    }
