9caedb06f2a37e712dd845f0fe1091fdce6e32ba
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-09 22:11:19 +0000

    create api sign in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -13,3 +13,14 @@ export const signup = async (req, res, next) => {
     next(error);
    }
 }
+
+
+export const signin = async (req, res, next) => {
+   const { email, password } = req.body;
+   try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) return 
+   } catch (error) {
+      next(error)
+   }
+}
