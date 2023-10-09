8012cdd04d4f6bc9ae6784462e46d403b08a342d
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-09 23:21:50 +0000

    insert expiry

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -28,8 +28,11 @@ export const signin = async (req, res, next) => {
       const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
 
       const { password: hashedPassword, ...rest } = validUser._doc;
+
+      const expiryDate = new Date(Date.now() + 3600000 ) // 1 hour
+
       res
-         .cookie('access_token', token, {httpOnly: true})
+         .cookie('access_token', token, {httpOnly: true, expires: expiryDate})
          .status(200)
          .json(rest)
    } catch (error) {
