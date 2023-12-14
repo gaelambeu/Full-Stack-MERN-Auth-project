df4bd41ccc1a9c905f890b1325075b960bc7bb9a
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-14 23:35:19 +0000

    header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
-const Header = () => {
+
+export default function Header() {
   return (
-    <div>Header</div>
+    <div className='bg-slate-200'>
+        <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
+            <Link to='/'>
+                <h1 className="font-bold">Auth App</h1>
+            </Link>
+            
+            <ul className='flex gap-4'>
+                <Link to='/'>
+                    <li>Home</li>
+                </Link>
+                <Link to='/about'>
+                    <li>About</li>
+                </Link>
+                <Link to='/sign-in'>
+                    <li>Sign In</li>
+                </Link>
+            </ul>
+        </div>
+    </div>
   )
 }
-
-export default Header
