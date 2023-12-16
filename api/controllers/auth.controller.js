c75913e395d582e7e296c3d6a90546ddfcd0f933
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-16 23:41:22 +0000

    short

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -51,6 +51,20 @@ export const google = async (req, res, next) => {
       } else {
          const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8)
          const hashedPassword = bcryptjs.hashSync(generatedPassword, 10)
+         const newUser = new User({ 
+            username: req.body.name.split("").join("").toLowerCase() + Math.floor(Math.random * 10000), 
+            email: req.body.email,
+            password: hashedPassword,
+            profilePicture: req.body.photo
+         })
+         await newUser.save()
+         const token = jwt.sign({ id: newUser._id}, process.env.JWT_SECRET)
+         const { password: hashedPassword2, ...rest } = newUser._doc;
+         const expiryDate = new Date(Date.now() + 3600000) //hour
+         res.cookie('access_token', token, {
+            httpOnly: true,
+            expires: expiryDate,
+         })
       }
    } catch (error) {
       next(error)
