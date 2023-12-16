f01d476de47d07c990a058a95fcbae75430d4c11
GAEL AMBEU <ambeugael128@gmail.com>
2023-12-16 19:06:49 +0000

    short

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,12 @@
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth"
 import { app } from "../firebase"
+import { useDispatch } from "react-redux"
+import { signInSuccess } from "../redux/user/userSlice";
 
 
 export default function OAuth() {
+    const dispatch = useDispatch();
+
     const handleGoogleClick = async () => {
         try {
             const provider = new GoogleAuthProvider()
@@ -21,6 +25,7 @@ export default function OAuth() {
                 })
             })
             const data = await res.json();
+            dispatch(signInSuccess(data));
         } catch (error) {
             console.log("could not login with google", error);
         }
