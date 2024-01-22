dc766ad5177eec53460302657b0f2aac2db79223
GAEL AMBEU <ambeugael128@gmail.com>
2024-01-22 13:45:32 +0000

    shop

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,11 +3,8 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (   
-    <div className="video-background">
-      <video autoPlay muted loop id="myVideo">
-        <source src={videoURL} type="video/mp4" />
-      </video>
-    </div>
-      
+    <div className="w-full bg-fixed" style="background-image: url(https://i.pinimg.com/564x/06/7a/e5/067ae5637a1a53edca4da823800090c9.jpg)">
+
+    </div>      
   );
 }
